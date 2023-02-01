56360b8340bedf91a8bf84e574dcd3c96d2f952d
Zaid kamil <xaidmetamorphos@gmail.com>
2023-02-01 20:10:47 +0530

    io

---
+++ info.py
@@ -0,0 +1,12 @@
+name = input('Enter Name 😀')
+dob = input("Enter Date of Birth 🎂")
+city = input("Enter City 🏙")
+college = input("Enter College 🏫")
+fav_color = input("Enter Favorite Color 🎨")
+fav_food = input("Enter Favorite Food 🍕")
+fav_animal = input("Enter Favorite Animal 🐶")
+fav_movie = input("Enter Favorite Movie 🎥")
+fav_song = input("Enter Favorite Song 🎵")
+
+print(name, dob, city, college, fav_color, 
+      fav_food, fav_animal, fav_movie, fav_song)
---
+++ si.py
@@ -0,0 +1,5 @@
+p = int(input("Pricipal = "))
+r = int(input("Rate = "))
+t = int(input("Time = "))
+si = (p*r*t)/100
+print(si)

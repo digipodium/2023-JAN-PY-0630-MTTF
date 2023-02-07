2a88f32f896e294c6b69ced87ed73e7a3ef75967
Zaid kamil <xaidmetamorphos@gmail.com>
2023-02-07 19:46:32 +0530

    muted class

---
+++ t1.py
@@ -0,0 +1,11 @@
+# turtle is a graphic library
+from turtle import *
+
+speed('slowest')
+left(90)        # turn left 90 deg
+forward(120)    # then move forwared 120 units
+left(90)
+forward(120)
+left(90)
+forward(120)
+mainloop()
---
+++ t3.py
@@ -0,0 +1,15 @@
+# the power of not repeating
+from turtle import *
+
+side = 6
+for i in range(side):
+    pencolor('blue')
+    fd(100)
+    lt(360/side)
+    pencolor('red')
+    dot(30)
+    for i in range(side):
+        fd(50)
+        lt(360/side)
+
+mainloop()

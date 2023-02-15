ac568339ad5a222cad4b620179ec424bd037a752
Zaid Kamil <xaidmetamorphos@gmail.com>
2023-02-15 20:00:02 +0530

    games

---
+++ game1.py
@@ -0,0 +1,34 @@
+import pgzrun
+
+box = Rect((50,50), (150, 100))
+box2 = Rect((600,50), (100,100))
+bvx = 4
+b2vx = -3
+
+def draw():
+    screen.fill('black')
+    screen.draw.filled_rect(box, 'yellow')
+    screen.draw.filled_rect(box2, 'red')
+
+def update():
+    global bvx, b2vx
+    box.x += bvx
+    box2.x += b2vx
+    # check for collision
+    if box.colliderect(box2):
+        bvx = -bvx
+        b2vx = -b2vx
+        sounds.hat.play()
+    # check for wall collision box
+    if box.left < 0:
+        bvx = -bvx
+        sounds.cling.play()
+
+    # check for wall collision of box2
+    if box2.right > 800:
+        b2vx = -b2vx
+        sounds.hat.play()
+
+    
+
+pgzrun.go()
